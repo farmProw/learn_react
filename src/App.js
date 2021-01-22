5c5039e0975e0805aec49e0f389eda25d873ce7a
Vitalka <188hardbass@gmail.com>
2021-01-22 18:33:40 +0200

    first comit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './../src/components/Header'
+import Navbar from './../src/components/Navbar'
+import Profile from './../src/components/Profile'
+
+const App = () => {
+  return (
+    <div className="app-wrapper">
+      <Header />
+      <Navbar/>
+      <Profile/>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+const Header =()=>{
+   return(
+      <header className="header">
+      <img src="https://memegenerator.net/img/instances/64572741.jpg">
+      </img>
+    </header>
+   )
+}
+export default Header
